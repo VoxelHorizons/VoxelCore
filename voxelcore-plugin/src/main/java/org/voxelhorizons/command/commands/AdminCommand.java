f85d92c70d6dac0cfe8635a3f039038c902ab9ce
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-12 11:31:52 +1000

    Move admin command into plugin module

---
+++ voxelcore-plugin/src/main/java/org/voxelhorizons/command/commands/AdminCommand.java
@@ -0,0 +1,56 @@
+package org.voxelhorizons.command.commands;
+
+import org.bukkit.command.CommandSender;
+import org.voxelhorizons.VoxelCore;
+import org.voxelhorizons.command.SubCommand;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AdminCommand implements SubCommand {
+
+    private final Map<String, SubCommand> children = new HashMap<String, SubCommand>();
+
+    public AdminCommand() {
+        register(new ReloadCommand());
+    }
+
+    private void register(SubCommand command) {
+        children.put(command.getName(), command);
+        for (String alias : command.getAliases()) {
+            children.put(alias, command);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "admin";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Collections.singletonList("a");
+    }
+
+    @Override
+    public String getPermission() {
+        return VoxelCore.getInstance().getDescription().getName().toLowerCase() + ".admin";
+    }
+
+    @Override
+    public boolean playerOnly() {
+        return false;
+    }
+
+    @Override
+    public Map<String, SubCommand> getChildren() {
+        return children;
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        sender.sendMessage("Reloading " + VoxelCore.getInstance().getDescription().getName() + "...");
+    }
+}
