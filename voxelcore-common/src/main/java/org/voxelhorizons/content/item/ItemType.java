31aae88c66333c6f63074d1d18376a35799c0be3
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-12 11:29:20 +1000

    Add shared item type model

---
+++ voxelcore-common/src/main/java/org/voxelhorizons/content/item/ItemType.java
@@ -0,0 +1,14 @@
+package org.voxelhorizons.content.item;
+
+public enum ItemType {
+    ITEM,
+    BLOCK,
+    TOOL,
+    WEAPON,
+    ARMOR,
+    FOOD,
+    FURNITURE,
+    VEHICLE,
+    GUI,
+    MISC
+}
