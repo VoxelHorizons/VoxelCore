92c78a9555530a9483980d6b7c144b983e1248ab
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-12 11:30:01 +1000

    Move version adapter contract to common module

---
+++ voxelcore-common/src/main/java/org/voxelhorizons/platform/VersionAdapter.java
@@ -0,0 +1,9 @@
+package org.voxelhorizons.platform;
+
+import org.voxelhorizons.platform.item.ItemPlatformAdapter;
+
+public interface VersionAdapter {
+    Version version();
+    Capabilities capabilities();
+    ItemPlatformAdapter items();
+}
