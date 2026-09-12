047e8d02bfdd64a3accabd99909201bfd3531822
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-12 11:33:46 +1000

    Add Minecraft 1.14 version adapter

---
+++ voxelcore-platform-v1_14/src/main/java/org/voxelhorizons/platform/v1_14/v1_14_VersionAdapter.java
@@ -0,0 +1,33 @@
+package org.voxelhorizons.platform.v1_14;
+
+import org.bukkit.plugin.Plugin;
+import org.voxelhorizons.platform.Capabilities;
+import org.voxelhorizons.platform.Version;
+import org.voxelhorizons.platform.VersionAdapter;
+import org.voxelhorizons.platform.item.ItemPlatformAdapter;
+
+public final class v1_14_VersionAdapter implements VersionAdapter {
+
+    private final Version version;
+    private final ItemPlatformAdapter itemAdapter;
+
+    public v1_14_VersionAdapter(Version version, Plugin plugin) {
+        this.version = version;
+        this.itemAdapter = new v1_14_ItemAdapter(plugin);
+    }
+
+    @Override
+    public Version version() {
+        return version;
+    }
+
+    @Override
+    public Capabilities capabilities() {
+        return new Capabilities(true, true, false, false, false, false);
+    }
+
+    @Override
+    public ItemPlatformAdapter items() {
+        return itemAdapter;
+    }
+}
