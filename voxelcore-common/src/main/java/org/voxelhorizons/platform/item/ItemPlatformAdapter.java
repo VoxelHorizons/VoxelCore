b3fe2b742d6373da578e1be12274a7946e361d35
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-12 16:53:45 +1000

    Pass render allocations into item adapters

--- voxelcore-common/src/main/java/org/voxelhorizons/platform/item/ItemPlatformAdapter.java
+++ voxelcore-common/src/main/java/org/voxelhorizons/platform/item/ItemPlatformAdapter.java
@@ -3,11 +3,17 @@ package org.voxelhorizons.platform.item;
 import org.bukkit.inventory.ItemStack;
 import org.voxelhorizons.content.ContentID;
 import org.voxelhorizons.content.item.ItemDefinition;
+import org.voxelhorizons.content.render.RenderAllocation;
 
 import java.util.Optional;
 
 public interface ItemPlatformAdapter {
     ItemStack createItem(ItemDefinition definition, int quantity);
+
+    default ItemStack createItem(ItemDefinition definition, int quantity, RenderAllocation allocation) {
+        return createItem(definition, quantity);
+    }
+
     Optional<ContentID> getContentId(ItemStack stack);
     ItemStack setContentId(ItemStack stack, ContentID id);
 }
