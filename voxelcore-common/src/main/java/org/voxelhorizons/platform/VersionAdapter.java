eb057fba18aa6a5a09765b51d87fd73b09278468
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-26 22:14:13 +1200

    Expose optional packet channel adapter

--- voxelcore-common/src/main/java/org/voxelhorizons/platform/VersionAdapter.java
+++ voxelcore-common/src/main/java/org/voxelhorizons/platform/VersionAdapter.java
@@ -1,9 +1,15 @@
 package org.voxelhorizons.platform;
 
 import org.voxelhorizons.platform.item.ItemPlatformAdapter;
+import org.voxelhorizons.platform.network.PacketChannelAdapter;
+import org.voxelhorizons.platform.network.UnsupportedPacketChannelAdapter;
 
 public interface VersionAdapter {
     Version version();
     Capabilities capabilities();
     ItemPlatformAdapter items();
+
+    default PacketChannelAdapter packets() {
+        return new UnsupportedPacketChannelAdapter();
+    }
 }
